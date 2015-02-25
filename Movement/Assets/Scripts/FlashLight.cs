54fdc6877d8f50b88f7787858782ecfa60c413bc
BenWolpoff <tearon42@gmail.com>
2015-02-24 18:12:39 -0600

    Setup
    Uploading the initial build of the game

---
+++ Movement/Assets/Scripts/FlashLight.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour {
+
+    public bool on = true;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // F toggles flashlight on and off
+       if (Input.GetKeyDown("f"))
+       {
+           on = !on;
+       }
+
+       // Getting E down will tilt flashlight up
+       if (Input.GetKeyDown("e"))
+       {
+           this.transform.Rotate(0, 0, 30);
+       }
+
+
+       // Getting E up will return it to normal
+
+       if (Input.GetKeyUp("e"))
+       {
+           this.transform.Rotate(0, 0, -30);
+       }
+
+       // Getting C down will tilt flashlight down
+       if (Input.GetKeyDown("c"))
+       {
+           this.transform.Rotate(0, 0, -30);
+       }
+
+
+       // Getting E up will return it to normal
+
+       if (Input.GetKeyUp("c"))
+       {
+           this.transform.Rotate(0, 0, 30);
+       }
+	
+	}
+}
---
+++ Movement/Assets/Scripts/LightBulb.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBulb : MonoBehaviour {
+
+    public GameObject flash;
+
+    public bool on;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Light switches on and off when Flashlight turns.
+
+        if (flash.gameObject.GetComponent<FlashLight>().on == true)
+        {
+            light.enabled = true;
+        }
+
+        if (flash.gameObject.GetComponent<FlashLight>().on == false)
+        {
+            light.enabled = false;
+        }
+
+        
+
+
+        
+    }
+
+    
+}
